345e1a2fcc281c4576217f7bb630f0b0122d7171
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2020-12-05 16:09:25 -0300

    Agrego base para vista gráfica.

--- cliente.py
+++ cliente.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>INDEX</h1>'
+	return render_template('index.html')
 
 @app.route('/hotels/')
 def hotels():
-	return '<h1>HOTELES</h1>'
+	return render_template('hotels.html')
 
 @app.route('/restaurants/')
 def restaurants():
-	return '<h1>RESTAURANTES</h1>'
+	return render_template('restaurants.html')
 
 if __name__ == '__main__':
 	app.run()
