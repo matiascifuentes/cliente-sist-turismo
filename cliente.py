9fd4fe603ced0635a3035415c84f3e0ab2e53f01
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2020-12-05 19:23:13 -0300

    Agrego uso de api para obtener hoteles.

--- cliente.py
+++ cliente.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+dominioApi = 'http://127.0.0.1:5000'
 
 @app.route('/')
 def index():
@@ -8,11 +10,14 @@ def index():
 
 @app.route('/hotels/')
 def hotels():
-	return render_template('hotels.html')
+	hoteles = requests.get(dominioApi + '/api/v1/hotels')
+	hoteles = hoteles.json()
+	#VERIFICAR RESPUESTA
+	return render_template('hotels.html',hoteles=hoteles['hotels'])
 
 @app.route('/restaurants/')
 def restaurants():
 	return render_template('restaurants.html')
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host='127.0.0.1',port='5001',debug=True)
