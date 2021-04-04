75ae35920ff6a2dd616707b0eb919efcf8899bbd
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-04-04 17:18:53 -0400

    Agrego funciones para login y logout

--- cliente.py
+++ cliente.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, current_user, login_user, logout_user
 import requests
+from models import users, LoginForm, get_user
+from werkzeug.urls import url_parse
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 dominioApi = 'http://127.0.0.1:5000'
 
 @app.route('/')
@@ -45,6 +51,33 @@ def atraction(cod_atraccion):
 	atraccion = requests.get(dominioApi + '/api/v1/atractions/' + cod_atraccion)
 	atraccion = atraccion.json()
 	return render_template('atraction.html',atraccion=atraccion['atraction'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = get_user(form.email.data)
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    for user in users:
+        if user.id == int(user_id):
+            return user
+    return None
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1',port='5001',debug=True)
